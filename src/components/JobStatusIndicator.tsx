22710cb7a596af916a33f1cde878e71fe7b64c71
Ozzie Gooen <ozzieagooen@gmail.com>
2025-06-29 19:23:10 -0700

    Minor sizing change

--- src/components/JobStatusIndicator.tsx
+++ src/components/JobStatusIndicator.tsx
@@ -12,9 +12,9 @@ export function JobStatusIndicator({
   showLabel = false 
 }: JobStatusIndicatorProps) {
   const sizeClasses = {
-    sm: "w-4 h-4",
-    md: "w-5 h-5",
-    lg: "w-6 h-6"
+    sm: "w-2 h-2",
+    md: "w-3 h-3",
+    lg: "w-4 h-4"
   };
 
   const labelSizeClasses = {
@@ -28,7 +28,7 @@ export function JobStatusIndicator({
       <div className="group relative flex items-center gap-2">
         <div className="relative">
           <div className={`${sizeClasses[size]} rounded-full bg-yellow-400`} />
-          <div className="invisible group-hover:visible absolute z-10 -top-8 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-gray-900 rounded whitespace-nowrap">
+          <div className="invisible group-hover:visible absolute z-10 -top-7 left-1/2 -translate-x-1/2 px-2 py-0.5 text-xs text-white bg-gray-900 rounded whitespace-nowrap">
             Queued
             <div className="absolute left-1/2 -translate-x-1/2 top-full w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-gray-900" />
           </div>
@@ -47,7 +47,7 @@ export function JobStatusIndicator({
       <div className="group relative flex items-center gap-2">
         <div className="relative">
           <div className={`${sizeClasses[size]} rounded-full bg-blue-400`} />
-          <div className="invisible group-hover:visible absolute z-10 -top-8 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-gray-900 rounded whitespace-nowrap">
+          <div className="invisible group-hover:visible absolute z-10 -top-7 left-1/2 -translate-x-1/2 px-2 py-0.5 text-xs text-white bg-gray-900 rounded whitespace-nowrap">
             Running
             <div className="absolute left-1/2 -translate-x-1/2 top-full w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-gray-900" />
           </div>
@@ -66,7 +66,7 @@ export function JobStatusIndicator({
       <div className="group relative flex items-center gap-2">
         <div className="relative">
           <div className={`${sizeClasses[size]} rounded-full bg-red-500`} />
-          <div className="invisible group-hover:visible absolute z-10 -top-8 left-1/2 -translate-x-1/2 px-2 py-1 text-xs text-white bg-gray-900 rounded whitespace-nowrap">
+          <div className="invisible group-hover:visible absolute z-10 -top-7 left-1/2 -translate-x-1/2 px-2 py-0.5 text-xs text-white bg-gray-900 rounded whitespace-nowrap">
             Failed
             <div className="absolute left-1/2 -translate-x-1/2 top-full w-0 h-0 border-l-4 border-r-4 border-t-4 border-l-transparent border-r-transparent border-t-gray-900" />
           </div>
