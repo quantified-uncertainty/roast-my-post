f63b1635e34d3d0ba23e61e2b924922751d555d2
Michael Ravits <michaelr524@gmail.com>
2025-10-18 17:25:49 +0000

    chore: Remove redundant Prettier config options

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,12 +1,5 @@
 module.exports = {
   trailingComma: "es5",
-  semi: true,
-  endOfLine: "lf",
-  printWidth: 160,
-  singleAttributePerLine: false,
-  objectWrap: "collapse",
-  singleQuote: true,
-  tabWidth: 2,
   plugins: ["prettier-plugin-tailwindcss"],
   overrides: [
     {
